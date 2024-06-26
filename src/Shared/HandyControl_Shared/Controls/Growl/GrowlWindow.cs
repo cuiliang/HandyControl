5edec92da162990e6ead3c4f6ad0190bf50df6f1
CuiLiang <cuiliang.bj@gmail.com>
2024-06-26 22:04:20 +0800

    GrowlWindow 避免出现在Alt+Tab 窗口列表中。

--- src/Shared/HandyControl_Shared/Controls/Growl/GrowlWindow.cs
+++ src/Shared/HandyControl_Shared/Controls/Growl/GrowlWindow.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Interop;
 using HandyControl.Tools;
 using HandyControl.Tools.Interop;
 
@@ -14,6 +16,7 @@ public sealed class GrowlWindow : Window
     {
         WindowStyle = WindowStyle.None;
         AllowsTransparency = true;
+        
 
         GrowlPanel = new StackPanel
         {
@@ -37,5 +40,38 @@ public sealed class GrowlWindow : Window
     }
 
     protected override void OnSourceInitialized(EventArgs e)
-        => InteropMethods.IntDestroyMenu(this.GetHwndSource().CreateHandleRef());
+    {
+        InteropMethods.IntDestroyMenu(this.GetHwndSource().CreateHandleRef());
+
+        HideFromAltTab(this);
+    }
+
+    /// <summary>
+    /// 避免在Alt+Tab中显示
+    /// </summary>
+    /// <param name="window"></param>
+    private void HideFromAltTab(Window window)
+    {
+        // 设置ShowInTaskbar属性为false
+        window.ShowInTaskbar = false;
+
+        // 获取窗口句柄
+        var hwnd = new WindowInteropHelper(window).Handle;
+
+        // 获取当前窗口样式
+        int exStyle = GetWindowLong(hwnd, GWL_EXSTYLE);
+
+        // 修改窗口样式，去掉WS_EX_APPWINDOW属性，添加WS_EX_TOOLWINDOW属性
+        SetWindowLong(hwnd, GWL_EXSTYLE, (exStyle & ~WS_EX_APPWINDOW) | WS_EX_TOOLWINDOW);
+    }
+
+    private const int GWL_EXSTYLE = -20;
+    private const int WS_EX_APPWINDOW = 0x00040000;
+    private const int WS_EX_TOOLWINDOW = 0x00000080;
+
+    [DllImport("user32.dll", SetLastError = true)]
+    private static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+
+    [DllImport("user32.dll")]
+    private static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
 }
